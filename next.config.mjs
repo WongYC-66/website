36bc30e68382365fa211d8e79b87dcd486ed4e84
WongYC-66 <yengchien@hotmail.com>
2024-10-01 02:55:16 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
 
     output: 'export',       // to generate static sites
-    basePath: "odin-personal-website",  // for github.io GH-PAGES
+    basePath: "website",  // for github.io GH-PAGES
 
     images: {
         remotePatterns: [
