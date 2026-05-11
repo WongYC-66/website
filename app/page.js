a28c6e2d5ebc5b5683c0459ee346a8990fabbd46
WongYC-66 <yengchien@hotmail.com>
2026-05-11 21:31:16 +0800

    Fix react cert image, Update Home Page intro

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
           {/* Short-description */}
           <p>
-            {` I'm a full-stack web developer with experience using React.js, Node.js, PostgreSQL and Ruby on Rails.`}
+            {` I'm a full-stack web developer with experience using React.js, Node.js, PostgreSQL and Ruby on Rails. Professionally, i develop feature with Java, SpringBoot, Angular.js, Typescript, and dive in CI/CD pipeline of Kubernetes, Docker, Jenkins.`}
           </p>
         </div>
 
@@ -39,8 +39,10 @@ export default function Home() {
         {/* Programming languages */}
         <div className="flex flex-col gap-4 bg-primary-content p-3 rounded-xl">
           <h2 className="font-bold">Programming</h2>
-          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=javascript,python,ruby' width={150} height={50} />
+          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=java,typescript,javascript,python,ruby' width={450} height={100} />
           <div className="flex flex-wrap gap-1">
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Java</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Typescript</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Javascript</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Python</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Ruby</span>
@@ -50,7 +52,7 @@ export default function Home() {
         {/* Front End Skill */}
         <div className="flex flex-col gap-4 bg-primary-content p-3 rounded-xl">
           <h2 className="font-bold">Front end</h2>
-          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=html,css,bootstrap,react,next,tailwindcss, daisy' width={500} height={50} />
+          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=html,css,bootstrap,react,next,tailwindcss,angular' width={750} height={100} />
           <div className="flex flex-wrap gap-1">
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">HTML</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">CSS</span>
@@ -58,13 +60,14 @@ export default function Home() {
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">React</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Next</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">TailwindCSS</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Angular</span>
           </div>
         </div>
 
         {/* BackEnd Skill */}
         <div className="flex flex-col gap-4 bg-primary-content p-3 rounded-xl">
           <h2 className="mb-2 font-bold">Back end</h2>
-          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=nodejs,express,mongo,postgresql,prisma,jest,rails' width={500} height={50} />
+          <Image alt='front-end-skills' src='https://skillicons.dev/icons?i=nodejs,express,mongo,postgresql,prisma,jest,rails,spring,docker,kubernetes,jenkins' width={750} height={100} />
           <div className="flex flex-wrap gap-1">
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Node.js</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">express</span>
@@ -73,6 +76,10 @@ export default function Home() {
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Prisma</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Jest</span>
             <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Ruby on Rails</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">SpringBoot</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Docker</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Kubernetes</span>
+            <span className="px-3 py-0.5 rounded-3xl bg-primary text-primary-content">Jenkins</span>
           </div>
         </div>
 
@@ -83,7 +90,25 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-4 px-5">
         <h2 className="font-bold text-2xl self-start">About me</h2>
 
-        <p className="">{`I graduated in 2019 with a degree in Mechanical Engineering and worked in the field for four years. I eventually realized that I had a stronger interest in software development, so I decided to make a career switch. I’ve been teaching myself how to program, primarily through FreeCodeCamp and The Odin Project. These online programs included many hands-on projects that helped me develop my skills and solidify my passion for web development. I’m now eager to continue growing as a PERN stack developer by working on personal projects and seeking opportunities to advance my career in software engineering.`}</p>
+        <p className="">
+          {`
+          I graduated in 2019 with a degree in Mechanical Engineering and worked in the field for four years. 
+          I eventually realized that I had a stronger interest in software development, so I decided to make a career switch. 
+          I’ve been teaching myself how to program, primarily through FreeCodeCamp and The Odin Project. 
+          These online programs included many hands-on projects that helped me develop my skills and solidify my passion for web development. 
+          I’m now eager to continue growing as a PERN stack developer by working on personal projects and seeking opportunities to advance my career in software engineering.
+          `}
+        </p>
+
+        <p className="">
+          {`
+          Along my professional journey in software, I developed new features, integrating front-end UI lazily (optimized) with backend APIs. 
+          Designed Java stateful service and solved multi-threaded concurrency challenges. 
+          Worked with containerized java app deployment using Docker and Kubernetes. 
+          Meanwhile, a clean code practitionist, who cares about code maintainability and scalability.
+          Now, i'm a Certified React/Angular Developer, problem solver. :)
+          `}
+        </p>
 
         {/* Button My Certifications */}
         <Link href='/certifications' className="w-full">
